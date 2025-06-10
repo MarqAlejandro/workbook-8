dae84911887f126568b1d0c1a6146b4394a2d26c
MarqAlejandro <alejandromarq3@gmail.com>
2025-06-10 15:53:47 -0700

    successfully connected to db

--- Workshop_cardealershipanddatabase/src/main/java/com/plurasight/Main.java
+++ Workshop_cardealershipanddatabase/src/main/java/com/plurasight/Main.java
@@ -1,7 +1,32 @@
 package com.plurasight;
 
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        String url = "jdbc:sqlserver://skills4it.database.windows.net:1433;" +
+                "database=Courses;" +
+                "user=user419@skills4it;" +
+                "password=YearupSecure2025!;" +
+                "encrypt=true;" +
+                "trustServerCertificate=false;" +
+                "loginTimeout=30;";
+
+        try (Connection conn = DriverManager.getConnection(url);
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT * FROM MARQ.DEALERSHIPS")) {
+
+            while (rs.next()) {
+                System.out.println("ID: " + rs.getInt("dealership_id") +
+                        ", Name: " + rs.getString("name"));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+
     }
 }
